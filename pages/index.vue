733acb7f6677a24accc6b334eb3007afa6f6b00f
natemallison <healthynate@gmail.com>
2020-09-28 14:42:05 -0400

    Setting up basic search with lodash debounce on the input.

--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,59 @@
 <template>
   <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">ef-search-app</h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+    <p>Everything Food Quick Search</p>
+    <input type="text" class="input" @input="searchDebounce($event)" />
+    <p>{{ searchResults }}</p>
+    <div v-for="(result, index) in searchResultsToDisplay" :key="index">
+      <p>{{ result }}</p>
+      <p>----------------</p>
     </div>
   </div>
 </template>
 
 <script>
+import debounce from "lodash/debounce";
+
 export default {
-  async created() {
-    const data = await this.$axios.$get("?q=apple");
-    console.log(data);
+  data() {
+    return {
+      loading: false,
+      searchResults: [],
+      searchError: false,
+    };
+  },
+  computed: {
+    searchResultsToDisplay() {
+      return this.searchResults.slice(0, 9);
+    },
+  },
+  methods: {
+    async getSearchResults(query) {
+      console.log(query);
+      this.searchError = false;
+      this.loading = true;
+      try {
+        const data = await this.$axios.$get(`?q=${query}`);
+        console.log(data);
+        this.searchResults = data.SearchResults;
+      } catch (error) {
+        this.error = true;
+      }
+
+      this.loading = false;
+    },
+    searchDebounce: debounce(function (event) {
+      this.getSearchResults(event.target.value);
+    }, 300),
   },
 };
 </script>
 
 <style>
-/* Sample `apply` at-rules with Tailwind CSS
+/* Sample `apply` at-rules with Tailwind CSS */
 .container {
-@apply min-h-screen flex justify-center items-center text-center mx-auto;
+  @apply min-h-screen flex justify-center items-start text-center mx-auto;
 }
-*/
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
-    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+.input {
+  @apply bg-gray-400 rounded appearance-none py-3 px-4 w-full;
 }
 </style>
