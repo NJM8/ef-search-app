908bdc7cdcb0b07ec1dc39fadf4c16b68548da95
natemallison <healthynate@gmail.com>
2020-09-30 10:21:08 -0400

    Refactored sortBy to work for any array/object, created loading text component.

---
+++ components/InputSelect.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="mt-4 flex flex-col lg:flex-row">
+    <span class="mr-4">{{ title }}: </span>
+    <select :value="value" name="input-select" class="input" @input="onInput">
+      <option disabled :selected="value === ''" value="">Select a value</option>
+      <option
+        v-for="(option, index) in selectOptions"
+        :key="index"
+        :value="option"
+      >
+        {{ option }}
+      </option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "InputSelect",
+  props: {
+    title: {
+      type: String,
+      default: "",
+    },
+    value: {
+      type: String,
+      default: "",
+    },
+    selectOptions: {
+      type: [Array, Object],
+      default: () => [],
+    },
+  },
+  methods: {
+    onInput(event) {
+      this.$emit("input", event.target.value);
+    },
+  },
+};
+</script>
---
+++ components/LoadingText.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="loader">
+    <p>{{ loadingMessage }}</p>
+  </div>
+</template>
+
+<script>
+// cheesy loading indicator, better served by a fancy loading spinner in a global component
+const BASE_SEARCH_MESSAGE = "Searching";
+
+export default {
+  name: "LoadingText",
+  data() {
+    return {
+      loadingMessage: BASE_SEARCH_MESSAGE,
+      loadingInterval: undefined,
+    };
+  },
+  created() {
+    this.loadingInterval = setInterval(() => {
+      this.loadingMessage = this.loadingMessage.includes("...")
+        ? BASE_SEARCH_MESSAGE
+        : this.loadingMessage.concat(".");
+    }, 350);
+  },
+  destroyed() {
+    clearInterval(this.loadingInterval);
+  },
+};
+</script>
+
+<style>
+.loader {
+  width: 200px;
+  @apply font-medium text-2xl text-left py-8;
+}
+</style>
